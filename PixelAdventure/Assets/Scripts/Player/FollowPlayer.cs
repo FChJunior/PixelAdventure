23a36581d3ffcef1bac721a85fba6af449a57978
Chagas <jniors75@gmail.com>
2024-04-11 08:43:50 -0300

    Follow PLayer

--- PixelAdventure/Assets/Scripts/Player/FollowPlayer.cs
+++ PixelAdventure/Assets/Scripts/Player/FollowPlayer.cs
@@ -6,12 +6,16 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] float minX, maxX;
+    [SerializeField] float minY, maxY;
     
     void LateUpdate()
     {
         transform.position = player.position;
         if(transform.position.x < minX) transform.position = new Vector3(minX, transform.position.y);
         if(transform.position.x > maxX) transform.position = new Vector3(maxX, transform.position.y);
+
+        if(transform.position.y < minY) transform.position = new Vector3(transform.position.x, minY);
+        if(transform.position.y > maxY) transform.position = new Vector3(transform.position.x, maxY);
         
     }
 }
